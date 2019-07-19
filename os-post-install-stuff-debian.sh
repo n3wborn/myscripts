0223659748e748e01449dd581af2a2f7b1e8df97
n3wborn <n3wborn@protonmail.com>
2019-07-19 10:18:38 +0200

    os-post-install-stuff-debian.sh init commit
    	First functions written :
    		- apt-update
    		- apt-upgrade
    		- install-softwares
    		- oh-my-install

---
+++ os-post-install-stuff-debian.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# os-post-install-stuff-debian.sh
+#
+# Script to automate most of what I need after a debian install
+
+apt-update() {
+    apt -y update
+}
+
+apt-upgrade() {
+    apt -y upgrade
+}
+
+install-softwares() {
+    # TODO: replace every software with a variable containing all of them
+    apt -y install  \
+        vim         \
+        git         \
+        zsh         \
+        # oh-my-zsh will be needed too
+        neovim      \
+        #python support only available in debian unstable
+        #python-neovim   \
+        #python3-neovim  \
+        wget        \
+        curl        \
+        screen      \
+        xclip
+    }
+
+oh-my-install() {
+    # https://github.com/robbyrussell/oh-my-zsh
+    # NOTE: I should install it for a normal user too
+    echo '-Y' | sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+}
